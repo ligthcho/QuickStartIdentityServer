f93ad8786df2bb582dda1ad3ab3cd97ee8b2a01b
lightcho <853358540@qq.com>
2018-08-16 16:10:51 +0800

    ..

---
+++ Client/Program.cs
@@ -0,0 +1,61 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+	class Program
+    {
+        static async Task Main(string[] args)
+        {
+			//// 从元数据中发现端口
+			//var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+			//// 请求令牌
+			//var tokenClient = new TokenClient(disco.TokenEndpoint,"client","secret");
+			//var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+			//if(tokenResponse.IsError)
+			//{
+			//	Console.WriteLine(tokenResponse.Error);
+			//	return;
+			//}
+
+			//Console.WriteLine(tokenResponse.Json);
+
+
+			// 从元数据中发现客户端
+			var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+
+			// 请求令牌
+			var tokenClient = new TokenClient(disco.TokenEndpoint,"ro.client","secret");
+			var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync("alice","password","api1");//使用用户名密码
+
+			if(tokenResponse.IsError)
+			{
+				Console.WriteLine(tokenResponse.Error);
+				return;
+			}
+
+			Console.WriteLine(tokenResponse.Json);
+			Console.WriteLine("\n\n");
+
+			// 调用api
+			var client = new HttpClient();
+			client.SetBearerToken(tokenResponse.AccessToken);
+
+			var response = await client.GetAsync("http://localhost:5001/identity");
+			if(!response.IsSuccessStatusCode)
+			{
+				Console.WriteLine(response.StatusCode);
+			}
+			else
+			{
+				var content = await response.Content.ReadAsStringAsync();
+				Console.WriteLine(JArray.Parse(content));
+			}
+			Console.ReadKey();
+		}
+	}
+}
--- QuickStartIdentityServer/Startup.cs
+++ QuickStartIdentityServer/Startup.cs
@@ -1,34 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace QuickStartIdentityServer
 {
-    public class Startup
+	public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
-        }
+		{//扩展在每次启动时，为令牌签名创建了一个临时密钥。在生成环境需要一个持久化的密钥
+			services.AddIdentityServer()
+				.AddDeveloperSigningCredential()
+				.AddInMemoryApiResources(Config.GetApiResources())//添加api资源
+				.AddInMemoryClients(Config.GetClients())///添加客户端
+				.AddTestUsers(Config.GetUsers()); //添加测试用户
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app,IHostingEnvironment env,ILoggerFactory loggerFactory)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			loggerFactory.AddConsole(LogLevel.Debug);
+			app.UseDeveloperExceptionPage();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
+			app.UseIdentityServer();
+
+		}
     }
 }
